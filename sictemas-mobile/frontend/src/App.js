b0cc336d7857932786ed264404a783e0215d35c4
YeiPihh <juanpablofreiregaleano@gmail.com>
2023-10-06 14:20:57 +0200

    tickets estilos

--- sictemas-mobile/frontend/src/App.js
+++ sictemas-mobile/frontend/src/App.js
@@ -1,9 +1,19 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from 'react-router-dom';
 import './App.css';
+import IndexComponent from './components/Indexcomponent/IndexComponent.jsx';
 
 function App() {
   return (
     <>
-    
+      <Router>
+        <Routes>
+          <Route path="/" element={<IndexComponent />} />
+        </Routes>
+      </Router>
     </>
   );
 }
